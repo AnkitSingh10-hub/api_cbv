3423ba20a7e3f29640d4ea82b32291fdede2cad7
ankit singh <ankitsinghtheweeknd@gmail.com>
2023-05-28 23:55:40 +0545

    first commit

---
+++ api/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
+from rest_framework import status
+from django.http import Http404
+
+
+# Create your views here.
+class CourseListView(APIView):
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        print(serializer.data)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CourseSerializer(data=request.data)
+        print(request.data, "Python data type request.data")
+
+        if serializer.is_valid():
+            serializer.save()
+            print(serializer.data, "Python data type into object")
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors)
+
+
+class CourseDetailView(APIView):
+    def get_course(self, pk):
+        try:
+            return Course.objects.get(id=pk)
+        except Course.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        course = self.get_course(pk)
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        course = self.get_course(pk)
+        serializer = CourseSerializer(course, data=request.data)
+        print(request.data, "Python data type request.data")
+
+        if serializer.is_valid():
+            serializer.save()
+            print(serializer.data, "Python data type into object")
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors)
+
+    def delete(self, request, pk):
+        self.get_course(pk).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
