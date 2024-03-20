b675712193dcd5dd42754c413e507d2ec7aecb57
Ankit <ankitsinghtheweeknd@gmail.com>
2024-03-20 12:04:39 +0545

    klk

--- api/views.py
+++ api/views.py
@@ -20,7 +20,6 @@ class CourseListView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            print(serializer.data, "Python data type into object")
             return Response(serializer.data, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors)
